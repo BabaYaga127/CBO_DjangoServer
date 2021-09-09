8c3b77fbc489c07014da2ff7ad1dec7b68d6c5f4
Bùi Quang Minh <minhdeptrai2709@gmail.com>
2021-09-10 06:29:10 +0700

    lol

--- django_server/back_up/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Profile
-
-class ProfileSerializer(serializers.HyperLinkedModelSerializer):
-    class Meta:
-        model = Profile
-        fileds = ('ID', 'backgroundImg', 'avatar', 'name', 'gmail', 'password')
